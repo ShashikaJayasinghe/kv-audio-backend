6c26a51c795e0bdc939458f368620def594db50c
Shashika Jayasinghe <shashikajay72@gmail.com>
2025-02-17 18:14:52 +0530

    Initialized the backend repo and create authentication

---
+++ controllers/productController.js
@@ -0,0 +1,27 @@
+import Product from "../models/product.js";
+
+export function addProduct (req, res) {
+    console.log(req.user)
+    if (req.user == null) {     //if not have token we can't create product if not user 
+        res.status(401).json({
+            message : "please login and try again"
+        })
+        return
+    }
+    if (req.user.role != "admin") {
+        res.status(403).json({
+            message : "You are not authorized to perform this action"
+        })
+        return
+    }
+
+
+    const data = req.body;
+    const newProduct = new Product(data);
+
+    newProduct.save().then(()=>{
+        res.json({message : "Product added successfully"});
+    }).catch((error)=>{
+        res.status(500).json({error : "product addition failed"})
+    })
+}
---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express"
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import userRouter from "./routes/userRouter.js";
+import productRouter from "./routes/productRouter.js";
+import jwt from "jsonwebtoken";
+
+const app = express();          
+
+app.use(bodyParser.json());     //middleware
+app.use((req,res,next)=>{
+    let token = req.header("Authorization")
+    
+    if (token != null) {
+        token = token.replace("Bearer ","");
+        jwt.verify(token,"kv-secret-89",(err,decoded)=>{
+            if (!err) {
+                req.user = decoded;
+            }
+        });
+    }
+    next();
+})     
+
+const mongoUrl = "mongodb+srv://admin:123@cluster0.shgk9.mongodb.net/prods?retryWrites=true&w=majority&appName=Cluster0"
+mongoose.connect(mongoUrl);
+const connection = mongoose.connection        
+connection.once("open", ()=> {      
+    console.log("MongoDB connection established successfully")
+})
+
+
+
+app.use("/api/users",userRouter)        //plug  or   end points userrouter => controller => models
+app.use("/api/products", productRouter)
+
+app.listen (3000,()=> {
+    console.log("Server is running on port 3000")
+})
